2488cee2fa61ccc16bb74669a7c8ac7305280580
EC2 Default User <ec2-user@ip-172-31-86-128.ec2.internal>
2022-04-07 15:09:32 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  get 'homes/top'
+   resources :books
+  root :to => 'homes#top'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
